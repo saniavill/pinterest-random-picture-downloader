b48468fcab6ab312f40676cc715209fa6e58fa9d
Sania La <saniourry@gmail.com>
2024-07-18 18:18:22 +0330

    Update pinterest.py

--- pinterest.py
+++ pinterest.py
@@ -3,13 +3,13 @@ import os
 import requests
 import random
 
-with open('pinz3.json', encoding="UTF-8") as f: #open your json file
+with open('pinz3.json', encoding="UTF-8") as f: #open your json file. mine contains a random board's info. namely the url pics, description etc
     data = json.load(f)
 
-picture_links = []
+picture_links = [] #for all the links
 
 for item in data['pictures']:
-    picture_links.append(item["image/url"])
+    picture_links.append(item["image/url"]) #access the links in the json file and put them inside the list
 
 def _random_link_picker():
     randlink = random.choice(picture_links)
@@ -17,9 +17,9 @@ def _random_link_picker():
 
 
 def download_image():
-    filename = 'picture.jpg'
-    image_url = _random_link_picker()
-    image_response = requests.get(image_url, stream=True)
+    filename = 'picture.jpg' #to waste less storage, every time you run this file the new image replaces the image that was downloaded before
+    imageurl = _random_link_picker()
+    image_response = requests.get(imageurl, stream=True)
     if image_response.status_code == 200:
         with open(filename, "wb") as image_file:
             for chunk in image_response:
